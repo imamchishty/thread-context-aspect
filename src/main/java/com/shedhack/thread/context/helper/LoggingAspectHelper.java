4bbf816c3473a899de0f7b36e9ba13bc67b79182
Imam Chishty <imam.chishty@emc.com>
2016-04-04 21:43:08 +0400

    Logging Aspect helper added (work in progress).

---
+++ src/main/java/com/shedhack/thread/context/helper/LoggingAspectHelper.java
@@ -0,0 +1,17 @@
+package com.shedhack.thread.context.helper;
+
+import java.util.Map;
+
+/**
+ * Created by imamchishty on 04/04/2016.
+ */
+public class LoggingAspectHelper implements AspectHelper {
+
+    public String getId() {
+        return null;
+    }
+
+    public Map<String, Object> getContext() {
+        return null;
+    }
+}
